bff01a03fd1ac8327750439941bbcbdc30c6e500
purpleNoah7 <150533608+purpleNoah7@users.noreply.github.com>
2024-02-04 20:00:20 -0500

    Update ProjectPage.jsx

--- app/components/ProjectPage.jsx
+++ app/components/ProjectPage.jsx
@@ -3,7 +3,7 @@ import Project from "./Project";
 export default function ProjectPage() {
   return (
     <div className="h-screen w-full flex flex-col items-center gap-16 ">
-      <h2 className="text-white text-6xl font-black">Proyectos</h2>
+      <h2 className="text-white text-6xl font-black">Project</h2>
       <div className="flex flex-col w-full items-center pt-10 md:pt-1 justify-center h-full">
         <Project></Project>
       </div>
